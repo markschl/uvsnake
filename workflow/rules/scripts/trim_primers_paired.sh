8a5889d542ac5683887ad69701628fd449cc9efa
markschl <markschl19@gmail.com>
2023-09-20 09:53:14 +0200

    Adjust primer trimming: add --no-indels
    * --no-indels makes sense with Illumina data that have mostly
      substitution, but not InDel errors. At the same time, the
      USEARCH algorithms are designed for Illumina data
    (https://www.drive5.com/usearch/manual/faq_unoise_not_illumina.html).
    We may allow this to be configured in the future.
    * Forward/reverse read processing is now done sequentially,
      not in two parallel processes.

--- workflow/rules/scripts/trim_primers_paired.sh
+++ workflow/rules/scripts/trim_primers_paired.sh
@@ -3,11 +3,6 @@
 exec &> "${snakemake_log[0]}"
 set -xeuo pipefail
 
-
-# some variables
-short_file="${snakemake_output[short]%.zst}"
-sample="${snakemake_wildcards[sample]}"
-
 # we know that the output files are all in the same directory, therefore
 # we can use the dirname of the first file
 split_files=("${snakemake_output[by_primers]}")
@@ -17,38 +12,41 @@ mkdir -p "$outdir"
 # First, recognize forward and reverse primers, which are supplied as 
 # FASTA files containing one or more sequences. If > 1 sequences,
 # we expect a primer mixture, where all sequences start or end at the same
-# location of the locus.
+# position within the locus.
 #
-# This command:
-# * decompresses the Zstandard-compressed merged reads
-# * recognizes the forward primers and writes the primer name to the header
-# * recognizes the reverse primers and writes the primer name to the header,
-#   while at the same time dropping reads that are too short
-# * distributes the reads by primer combination into different files
-# The logs are stored in extra files, which can be parsed by MultiQC
-# TODO: MultiQC may accept --json output someday
-# TODO: default to --no-indels?
-zstd -dcq "${snakemake_input[seq]}" \
-  |   # trim forward primers, adding fwd=<primer_name> to header
+# The logs are stored in extra files, which can be parsed by MultiQC.
+# TODO: MultiQC may accept --json output someday https://github.com/ewels/MultiQC/issues/1933
+#
+# Recognize the forward primers and write the primer name to the header
+fwd_out="$outdir/__trim_fwd.fastq"
+short_file="${snakemake_output[short]%.zst}"
+zstd -dcq "${snakemake_input[seq]}" |
   cutadapt - \
     -g "file:${snakemake_input[fprimers]}" \
     --suffix ' fwd={name}' \
     --error-rate "${snakemake_params[max_error_rate]}" \
     --overlap "${snakemake_params[min_overlap]}" \
-    2> "${snakemake_output[fwd_log]}" \
-    |  # trim reverse primers, adding rev=<primer_name> to header
-  cutadapt - \
+    --no-indels \
+    -o "$fwd_out" \
+    &> "${snakemake_output[fwd_log]}" 
+  
+# Recognize the reverse primers and 
+# distribute the reads by primer combination into different files
+cutadapt "$fwd_out" \
     -a "file:${snakemake_input[rprimers_rev]}" \
     --suffix ' rev={name}' \
     --minimum-length "${snakemake_params[min_length]}" \
     --too-short-output "$short_file" \
     --error-rate "${snakemake_params[max_error_rate]}" \
     --overlap "${snakemake_params[min_overlap]}" \
+    --no-indels \
     2> "${snakemake_output[rev_log]}" \
     |  # use 'seqtool' to split by primer combination (names in header)
   st split --fq -o "$outdir/{a:fwd}...{a:rev}.fastq"
 
-# Second, compress the files
+rm "$fwd_out"
+
+# Compress the files
 # We want one ZSTD archive for every requested primer combination,
 # even the ones that don't have any reads.
 shopt -s nullglob
@@ -56,7 +54,7 @@ for comb in ${snakemake_params[primer_comb]}; do
     if [ -f "$outdir/$comb.fastq" ]; then
         zstd --rm -qf "$outdir/$comb.fastq"
     else
-        echo "No sequences with both forward and reverse primer ($comb) were found in sample '$sample'"
+        echo "No sequences with both forward and reverse primer ($comb) were found in sample '${snakemake_wildcards[sample]}'"
         echo -n | zstd -cq > "$outdir/$comb.fastq.zst"
     fi
 done
@@ -74,8 +72,8 @@ for f in "$outdir/"*...no_adapter.fastq; do zstd --rm -qf "$f"; done
 # Finally, handle trimming statistics:
 # add sample name to cutadapt logs:
 # this hack is needed to tell MultiQC the sample name
-sed -i -E "s/(Command line parameters[^$]+$)/\1 $sample.fastq.gz/g" "${snakemake_output[fwd_log]}"
-sed -i -E "s/(Command line parameters[^$]+$)/\1 $sample.fastq.gz/g" "${snakemake_output[rev_log]}"
+sed -i -E "s/(Command line parameters[^$]+$)/\1 ${snakemake_wildcards[sample]}.fastq.gz/g" "${snakemake_output[fwd_log]}"
+sed -i -E "s/(Command line parameters[^$]+$)/\1 ${snakemake_wildcards[sample]}.fastq.gz/g" "${snakemake_output[rev_log]}"
 # general read statistics
 if grep -q "No reads processed!" "${snakemake_output[fwd_log]}"; then
   n=0
